e65880cbc3d389c3e68768a6f2ed3d31e98e9d84
Sergio Peluzzi <sergiopeluzzi@gmail.com>
2020-05-25 21:01:33 -0400

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Criar pasta para guardar os pacotes NPM globais
+ * $ mkdir .npm-global
+ * 
+ * Informar ao NPM a nova pasta global
+ * npm config set prefix '~/.npm-global'
+ * 
+ * Editar ou criar o profile do usuário / nano ou vim ou mcedit
+ * sudo nano ~/.profile
+ * 
+ * Adicionar linha de código
+ * export PATH=~/.npm-global/bin:$PATH
+ * 
+ * Atualiza as variáveis de ambiente
+ * source ~/.profile
+ * 
+ * 
+ * npm i -g typescript
+ * 
+ */
+
+
+ /**
+  * 
+  * 1 - Criar o projeto do AdonisJs
+  * 2 - Criar o projeto do React-Native
+  * 3 - Criar um repositório para cada projeto no Github / GitLab / GitBucket
+  * 4 - Mandar os projetos para os repos e enviar o link * 
+  * 
+  */
